12f430eba3d16077d8f21e3d7ad117891cc437f7
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2023-12-28 13:38:49 +0200

    First Push

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+import "./index.css";
+import App from "./App";
+import React from "react";
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
